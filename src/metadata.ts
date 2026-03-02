f53d545b7ef19a7103de2fe28780a406e468656e
Jan Slifka <jan.slifka@gmail.com>
2026-03-02 13:35:30 +0100

    1.1.0

--- src/metadata.ts
+++ src/metadata.ts
@@ -3,6 +3,6 @@ import { PluginMetadata } from '@ds-wizard/plugin-sdk/types'
 export const pluginMetadata: PluginMetadata = {
     uuid: '434b554b-0ba1-4272-b070-5682a1e13726',
     name: 'maDMP Importer',
-    version: '0.1.0',
+    version: '1.1.0',
     description: 'Importer for maDMPs according to RDA DMP Common Standard.',
 }
